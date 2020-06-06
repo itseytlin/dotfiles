3a2d55b650e33a58feeb7e887224272e1c2789f1
Ivan Tseytlin <itseytlin@haverford.edu>
2020-06-05 22:24:22 -0700

    Colored MAN pages

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,12 @@ fish_vi_key_bindings
 # Change user_color
 #set fish_color_user b9a4f9
 
+
+# Colored MAN pages
+set -x LESS_TERMCAP_mb (printf "\033[01;31m")
+set -x LESS_TERMCAP_md (printf "\033[01;31m")
+set -x LESS_TERMCAP_me (printf "\033[0m")
+set -x LESS_TERMCAP_se (printf "\033[0m")
+set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\033[0m")
+set -x LESS_TERMCAP_us (printf "\033[01;32m")
